3deaaf7640d750a767381df10f90f21dc20c8fc4
phill <phill@DESKTOP-5L40HG4>
2025-01-06 20:59:48 -0300

    Adicionar arquivos de projeto.

---
+++ Dima.Api/Program.cs
@@ -0,0 +1,47 @@
+using Dima.Api.Data;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var cnnStr = builder
+    .Configuration
+    .GetConnectionString("DefaultConnection")
+    ?? String.Empty;
+
+builder.Services.AddDbContext<AppDbContext>(
+    x =>
+    {
+        x.UseSqlServer(cnnStr);
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.CustomSchemaIds(n => n.FullName);
+});
+builder.Services.AddTransient<Handler>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.MapPost(
+    pattern: "/v1/categories",
+    handler: (CreateCategoryRequest request, Handler handler)
+    => handler.Handle(request))
+    .WithName("Categories: Create")
+    .WithSummary("Cria uma nova categoria")
+    .Produces<Response<Category>>();
+
+app.Run();
+
+
+
+
+
+
---
+++ Dima.Core/Requests/Categories/UpdateCategoryRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Requests.Categories
+{
+    public  class UpdateCategoryRequest : BaseRequest
+    {
+        [Required(ErrorMessage = "Título inválido")]
+        [MaxLength(80, ErrorMessage = "O Título deve conter até 80 caracteres.")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Descrição inválida")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
