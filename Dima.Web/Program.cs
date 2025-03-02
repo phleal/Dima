d7cb427037423cb6e53b84be7ec807b30a2f68ec
Phillip Leal <philliplealgs@gmail.com>
2025-03-02 19:19:17 -0300

    criacao register

--- Dima.Web/Program.cs
+++ Dima.Web/Program.cs
@@ -2,10 +2,26 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Dima.Web;
 using MudBlazor.Services;
+using Dima.Web.Security;
+using Microsoft.AspNetCore.Components.Authorization;
+using Dima.Core.Handlers;
+using Dima.Web.Handlers;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Configuration.BackendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<CookieHandler>();
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+builder.Services.AddScoped(x =>
+    (ICookieAuthenticationStateProvider)x.GetRequiredService<AuthenticationStateProvider>());
 
 builder.Services.AddMudServices();
 
@@ -13,5 +29,16 @@ builder.Services.AddHttpClient(Configuration.HttpClientName, opt =>
 {
     opt.BaseAddress = new Uri(Configuration.BackendUrl);
 }).AddHttpMessageHandler<CookieHandler>();
+
+//builder.Services.AddTransient<IAccountHandler, AccountHandler>();
+//builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+//builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+//builder.Services.AddTransient<IReportHandler, ReportHandler>();
+//builder.Services.AddTransient<IOrderHandler, OrderHandler>();
+//builder.Services.AddTransient<IStripeHandler, StripeHandler>();
+
+builder.Services.AddLocalization();
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("pt-BR");
 
 await builder.Build().RunAsync();
